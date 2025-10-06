27f4988e63b75fe18133579f25cf1b7774aecbce
uliana0814 <gabrieluliebani@gmail.com>
2025-10-05 22:38:18 -0300

    funcionalidades

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,29 @@
-// Código JS aqui!
+const cart_count_element = document.getElementById('cart-count');
+const add_buttons = document.querySelectorAll('.add');
+const remove_buttons = document.querySelectorAll('.remove');
+
+let cart_item_count = 0;
+
+function update_cart_count() {
+    cart_count_element.textContent = cart_item_count;
+}
+
+function add_to_cart() {
+    cart_item_count++;
+    update_cart_count(); 
+}
+
+function remove_from_cart() {
+    if (cart_item_count > 0) {
+        cart_item_count--; 
+        update_cart_count(); 
+    }
+}
+
+add_buttons.forEach(button => {
+        button.addEventListener('click', add_to_cart);
+});
+
+remove_buttons.forEach(button => {
+    button.addEventListener('click', remove_from_cart);
+});
